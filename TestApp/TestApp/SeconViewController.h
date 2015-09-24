8a784fd4f744d711b17b48c894aca6d8a8eeba2a
yangguojun <yangguojunaccp@163.com>
2015-09-24 15:04:29 +0800

    第二次提交
    第二次

---
+++ TestApp/TestApp/SeconViewController.h
@@ -0,0 +1,13 @@
+//
+//  SeconViewController.h
+//  TestApp
+//
+//  Created by 杨 国俊 on 15/9/24.
+//  Copyright (c) 2015年 sdzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SeconViewController : UIViewController
+
+@end
---
+++ TestApp/TestApp/SeconViewController.m
@@ -0,0 +1,37 @@
+//
+//  SeconViewController.m
+//  TestApp
+//
+//  Created by 杨 国俊 on 15/9/24.
+//  Copyright (c) 2015年 sdzy. All rights reserved.
+//
+
+#import "SeconViewController.h"
+
+@interface SeconViewController ()
+
+@end
+
+@implementation SeconViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
